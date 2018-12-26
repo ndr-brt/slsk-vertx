dc846233b892daf933aae166c05af631ae6d2086
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2018-12-26 18:04:44 +0100

    Handle some server messages

--- src/main/java/ndr/brt/slsk/ServerListener.kt
+++ src/main/java/ndr/brt/slsk/ServerListener.kt
@@ -2,7 +2,9 @@ package ndr.brt.slsk
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.Handler
 import io.vertx.core.buffer.Buffer
+import io.vertx.core.eventbus.EventBus
 import io.vertx.core.json.JsonObject
 import io.vertx.core.net.NetSocket
 import org.slf4j.LoggerFactory
@@ -31,26 +33,7 @@ class ServerListener(private val serverHost: String, private val serverPort: Int
             log.error("Error", cause)
         }
 
-        vertx.eventBus().consumer<Buffer>("ServerInputMessage") { message ->
-            val inputMessage = ProtocolBuffer(message.body())
-
-            val type = inputMessage.code()
-            when (type) {
-                1 -> {
-                    log.info("Recv Login")
-                    vertx.eventBus().publish("LoginResponded", LoginResponded(inputMessage.readByte().toInt() == 1, inputMessage.readString()).asJson())
-                }
-                64 -> {
-                    val numberOfRooms = inputMessage.readInt()
-                    log.info("Recv RoomList $numberOfRooms")
-                }
-                else -> {
-                    log.warn("Server message code $type unknown")
-                    vertx.eventBus().publish("UnknownMessage", "{}")
-                }
-            }
-
-        }
+        vertx.eventBus().consumer<Buffer>("ServerInputMessage", ServerInputMessage(vertx.eventBus()))
 
         vertx.eventBus().consumer<JsonObject>("LoginRequested") { message ->
             val event = message.body().mapTo(LoginRequested::class.java)
@@ -65,6 +48,68 @@ class ServerListener(private val serverHost: String, private val serverPort: Int
             log.info("Send file search message to server")
             socket.write(fileSearch.toChannel())
         }
+    }
+
+    class ServerInputMessage(private val eventBus: EventBus): Handler<io.vertx.core.eventbus.Message<Buffer>> {
+        private val log = LoggerFactory.getLogger(javaClass)
+
+        override fun handle(message: io.vertx.core.eventbus.Message<Buffer>) {
+            val inputMessage = ProtocolBuffer(message.body())
+
+            inputMessage.let(
+                when (inputMessage.code()) {
+                    1 -> login
+                    18 -> connectToPeer
+                    64 -> numberOfRooms
+                    69 -> privilegedUsers
+                    83 -> parentMinSpeed
+                    84 -> parentSpeedRatio
+                    104 -> wishListInterval
+                    else -> unknownMessage
+                }
+            )
+        }
+
+        private val unknownMessage: (ProtocolBuffer) -> Unit = { inputMessage ->
+            log.warn("Server message code ${inputMessage.code()} unknown")
+        }
+
+        private val wishListInterval: (ProtocolBuffer) -> Unit = { inputMessage ->
+            val wishlistInterval = inputMessage.readInt()
+            log.info("Recv WishlistInterval: $wishlistInterval")
+        }
+
+        private val parentSpeedRatio: (ProtocolBuffer) -> Unit = { inputMessage ->
+            val parentSpeedRatio = inputMessage.readInt()
+            log.info("Recv ParentSpeedRatio: $parentSpeedRatio")
+        }
+
+        private val parentMinSpeed: (ProtocolBuffer) -> Unit = { inputMessage ->
+            val parentMinSpeed = inputMessage.readInt()
+            log.info("Recv ParentMinSpeed: $parentMinSpeed")
+        }
+
+        private val privilegedUsers: (ProtocolBuffer) -> Unit = { inputMessage ->
+            val privilegedUsers = inputMessage.readInt()
+            log.info("Recv PrivilegedUsers: $privilegedUsers")
+        }
+
+        private val numberOfRooms: (ProtocolBuffer) -> Unit = { inputMessage ->
+            val numberOfRooms = inputMessage.readInt()
+            log.info("Recv RoomList: $numberOfRooms")
+        }
+
+        private val connectToPeer: (ProtocolBuffer) -> Unit = { inputMessage ->
+            val username = inputMessage.readString()
+            val type = inputMessage.readString()
+            log.info("Recv ConnectToPeer: username $username, type $type")
+        }
+
+        private val login: (ProtocolBuffer) -> Unit = { inputMessage ->
+            log.info("Recv Login")
+            eventBus.publish("LoginResponded", LoginResponded(inputMessage.readByte().toInt() == 1, inputMessage.readString()).asJson())
+        }
+
     }
 
 }
