a05f936a31314d34aa75f3649a2aac2372751585
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2018-12-29 15:50:51 +0100

    Extract methods

--- src/main/java/ndr/brt/slsk/peer/PeerListener.kt
+++ src/main/java/ndr/brt/slsk/peer/PeerListener.kt
@@ -7,7 +7,9 @@ import io.vertx.core.buffer.Buffer
 import io.vertx.core.eventbus.EventBus
 import io.vertx.core.net.NetClient
 import io.vertx.core.net.NetSocket
-import ndr.brt.slsk.*
+import ndr.brt.slsk.Address
+import ndr.brt.slsk.SearchResponded
+import ndr.brt.slsk.emit
 import ndr.brt.slsk.protocol.InputMessageHandler
 import ndr.brt.slsk.protocol.Protocol
 import ndr.brt.slsk.protocol.ProtocolBuffer
@@ -35,37 +37,42 @@ class PeerListener(address: Address, private val info: PeerInfo, peer: NetClient
             socket.handler(InputMessageHandler("PeerInputMessage-${info.username}", eventBus))
             eventBus.consumer<Buffer>("PeerInputMessage-${info.username}") { message ->
                 val inputMessage = ProtocolBuffer(message.body())
-                log.info("Received message from user ${info.username} code ${inputMessage.code()}")
                 when (inputMessage.code()) {
-                    9 -> {
-                        val unzip = inputMessage.decompress()
-                        val username = unzip.readString()
-                        val token = unzip.readToken()
-                        val resultsCount = unzip.readInt()
-                        val fileResult = mutableListOf<String>()
-                        for (i in 0 until resultsCount) {
-                            unzip.readByte()
-                            val filename = unzip.readString()
-                            val size1 = unzip.readInt()
-                            val size2 = unzip.readInt()
-                            unzip.readString()
-                            val attributesCount = unzip.readInt()
-                            for (j in 0 until attributesCount) {
-                                unzip.readInt()
-                                unzip.readInt()
-                            }
-                            fileResult.add(filename)
-                        }
-                        unzip.readByte()
-                        unzip.readInt()
-                        unzip.readInt()
-                        log.info("Recv FileSearchResult da $username")
-                        if (fileResult.size > 0) {
-                            eventBus.emit(SearchResponded(token, fileResult))
-                        }
-                    }
-                    else -> log.warn("Peer message unknown: ${inputMessage.code()}")
+                    9 -> fileSearchResult
+                    else -> unknownMessage
+                }.invoke(inputMessage)
+            }
+        }
+
+        private val unknownMessage: (ProtocolBuffer) -> Unit = { inputMessage ->
+            log.warn("Peer message unknown from ${info.username}: ${inputMessage.code()}")
+        }
+
+        private val fileSearchResult: (ProtocolBuffer) -> Unit = { inputMessage ->
+            val unzip = inputMessage.decompress()
+            val username = unzip.readString()
+            val token = unzip.readToken()
+            val resultsCount = unzip.readInt()
+            val fileResult = mutableListOf<String>()
+            for (i in 0 until resultsCount) {
+                unzip.readByte()
+                val filename = unzip.readString()
+                val size1 = unzip.readInt()
+                val size2 = unzip.readInt()
+                unzip.readString()
+                val attributesCount = unzip.readInt()
+                for (j in 0 until attributesCount) {
+                    unzip.readInt()
+                    unzip.readInt()
                 }
+                fileResult.add(filename)
+            }
+            unzip.readByte()
+            unzip.readInt()
+            unzip.readInt()
+            log.info("Recv FileSearchResult da $username")
+            if (fileResult.size > 0) {
+                eventBus.emit(SearchResponded(token, fileResult))
             }
         }
 
